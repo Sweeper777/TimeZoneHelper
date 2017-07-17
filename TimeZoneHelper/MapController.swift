b75aa867648c42a55b1453a6584cfb71ab66ef05
Sweeper777 <sumulang@gmail.com>
2017-07-17 11:46:18 +0800

    tried to add a crosshair to the map

--- TimeZoneHelper/MapController.swift
+++ TimeZoneHelper/MapController.swift
@@ -5,6 +5,7 @@ import RxGoogleMaps
 class MapController: UIViewController, GMSMapViewDelegate {
 
     @IBOutlet var map: GMSMapView!
+    @IBOutlet var crosshair: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
