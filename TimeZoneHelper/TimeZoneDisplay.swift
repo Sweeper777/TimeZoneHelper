3d4d7077caa4d1d35dc2785917f6b7a51a8199c4
Sweeper777 <sumulang@gmail.com>
2017-07-18 08:09:07 +0800

    copied boilerplate code from SO docs

--- TimeZoneHelper/TimeZoneDisplay.swift
+++ TimeZoneHelper/TimeZoneDisplay.swift
@@ -5,4 +5,32 @@ class TimeZoneDisplay: UIView {
     @IBOutlet var timeDisplay: UILabel!
     @IBOutlet var dayDisplay: UILabel!
     @IBOutlet var descriptionDisplay: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    private func setupView() {
+        let view = viewFromNibForClass()
+        view.frame = bounds
+        view.autoresizingMask = [
+            UIViewAutoresizing.flexibleWidth,
+            UIViewAutoresizing.flexibleHeight
+        ]
+        addSubview(view)
+    }
+    
+    private func viewFromNibForClass() -> UIView {
+        
+        let bundle = Bundle.main
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
 }
