49b427ac5166323bde91fcf86e8671f45c4922f0
Sweeper777 <sumulang@gmail.com>
2017-07-18 08:08:45 +0800

    added a basic TimeZoneDisplay class

---
+++ TimeZoneHelper/TimeZoneDisplay.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+@IBDesignable
+class TimeZoneDisplay: UIView {
+    @IBOutlet var timeDisplay: UILabel!
+    @IBOutlet var dayDisplay: UILabel!
+    @IBOutlet var descriptionDisplay: UILabel!
+}
