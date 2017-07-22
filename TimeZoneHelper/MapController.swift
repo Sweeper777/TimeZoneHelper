16c6d5678052f9dcc6e345ec58f7759fbd561641
Sweeper777 <sumulang@gmail.com>
2017-07-23 07:57:35 +0900

    added dispose bag

--- TimeZoneHelper/MapController.swift
+++ TimeZoneHelper/MapController.swift
@@ -1,12 +1,16 @@
 import UIKit
 import GoogleMaps
 import RxGoogleMaps
+import RxSwift
+import RxCocoa
 
 class MapController: UIViewController, GMSMapViewDelegate {
 
     @IBOutlet var map: GMSMapView!
     @IBOutlet var crosshair: UIImageView!
     @IBOutlet var timeZoneDisplay: TimeZoneDisplay!
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
