1e6ea5c8c09c61ad818cf155de4c00ffca0d9a20
Sweeper777 <sumulang@gmail.com>
2017-07-23 07:58:53 +0900

    added location property in TimeZoneDisplay

--- TimeZoneHelper/TimeZoneDisplay.swift
+++ TimeZoneHelper/TimeZoneDisplay.swift
@@ -1,10 +1,13 @@
 import UIKit
+import CoreLocation
+import TimeZoneLocate
 
 @IBDesignable
 class TimeZoneDisplay: UIView {
     @IBOutlet var timeDisplay: UILabel!
     @IBOutlet var dayDisplay: UILabel!
     @IBOutlet var descriptionDisplay: UILabel!
+    var location: CLLocation!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
