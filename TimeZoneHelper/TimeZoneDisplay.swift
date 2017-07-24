ea82f9aa2f305d6cc9a8cdcf025eeb24e3bbd4aa
Sweeper777 <sumulang@gmail.com>
2017-07-24 09:33:12 +0900

    added an UIActivityIndicator view

--- TimeZoneHelper/TimeZoneDisplay.swift
+++ TimeZoneHelper/TimeZoneDisplay.swift
@@ -7,6 +7,7 @@ class TimeZoneDisplay: UIView {
     @IBOutlet var timeDisplay: UILabel!
     @IBOutlet var dayDisplay: UILabel!
     @IBOutlet var descriptionDisplay: UILabel!
+    @IBOutlet var loadingIndicator: UIActivityIndicatorView!
     
     let geocoder = CLGeocoder()
     
