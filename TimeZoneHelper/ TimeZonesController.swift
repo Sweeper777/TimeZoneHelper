9530993182293acf4a95e6cc5a6d67431b5d4a10
Sweeper777 <sumulang@gmail.com>
2017-07-29 07:59:42 +0800

    added table view data source methods with simple implementations

--- TimeZoneHelper/ TimeZonesController.swift
+++ TimeZoneHelper/ TimeZonesController.swift
@@ -7,4 +7,17 @@ class TimeZonesController: UITableViewController {
     override func viewDidLoad() {
         timeZones = (try? Realm())?.objects(UserTimeZones.self)
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return timeZones.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        return cell
+    }
 }
