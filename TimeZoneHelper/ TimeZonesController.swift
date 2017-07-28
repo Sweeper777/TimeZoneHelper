5747bf0dc3fa4acace7bf171edbbc254b04458c8
Sweeper777 <sumulang@gmail.com>
2017-07-29 07:57:06 +0800

    loaded all the time zones from realm

--- TimeZoneHelper/ TimeZonesController.swift
+++ TimeZoneHelper/ TimeZonesController.swift
@@ -1,5 +1,10 @@
 import UIKit
+import RealmSwift
 
 class TimeZonesController: UITableViewController {
+    var timeZones: Results<UserTimeZones>!
     
+    override func viewDidLoad() {
+        timeZones = (try? Realm())?.objects(UserTimeZones.self)
+    }
 }
