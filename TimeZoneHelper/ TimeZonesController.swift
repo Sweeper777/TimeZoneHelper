e4895cf25aae81dc7722a9c768e652b7e3f3b316
Sweeper777 <sumulang@gmail.com>
2017-07-29 08:14:23 +0800

    made the stored time zones ordered

--- TimeZoneHelper/ TimeZonesController.swift
+++ TimeZoneHelper/ TimeZonesController.swift
@@ -5,7 +5,7 @@ class TimeZonesController: UITableViewController {
     var timeZones: Results<UserTimeZones>!
     
     override func viewDidLoad() {
-        timeZones = (try? Realm())?.objects(UserTimeZones.self)
+        timeZones = (try? Realm())?.objects(UserTimeZones.self).sorted(byKeyPath: "position")
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
--- TimeZoneHelper/UserTimeZone.swift
+++ TimeZoneHelper/UserTimeZone.swift
@@ -2,6 +2,7 @@ import RealmSwift
 
 class UserTimeZones: Object {
     dynamic var timeZoneInfo = Data()
+    dynamic var position = 0
     
     var timeZone: TimeZone {
         return NSKeyedUnarchiver.unarchiveObject(with: timeZoneInfo) as! TimeZone
