42db40f538b0369d37a7ca1e139d0789d35c34e4
Sweeper777 <sumulang@gmail.com>
2017-07-29 08:16:19 +0800

    added labelText property

--- TimeZoneHelper/UserTimeZone.swift
+++ TimeZoneHelper/UserTimeZone.swift
@@ -3,6 +3,7 @@ import RealmSwift
 class UserTimeZones: Object {
     dynamic var timeZoneInfo = Data()
     dynamic var position = 0
+    dynamic var labelText = ""
     
     var timeZone: TimeZone {
         return NSKeyedUnarchiver.unarchiveObject(with: timeZoneInfo) as! TimeZone
