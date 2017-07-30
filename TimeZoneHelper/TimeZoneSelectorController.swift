298e661f7a6e93bab6c249ac4f6465259cec7784
Sweeper777 <sumulang@gmail.com>
2017-07-30 08:24:25 +0800

    added four ways to select time zones

--- TimeZoneHelper/TimeZoneSelectorController.swift
+++ TimeZoneHelper/TimeZoneSelectorController.swift
@@ -1,5 +1,30 @@
 import Eureka
 
 class TimeZoneSelectorController: FormViewController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = NSLocalizedString("Select a Time Zone", comment: "")
+        form +++ Section(NSLocalizedString("select from...", comment: ""))
+        
+        <<< ButtonRow(tagMap) {
+            row in
+            row.title = NSLocalizedString("Map", comment: "")
+        }
+        
+        <<< ButtonRow(tagLocation) {
+            row in
+            row.title = NSLocalizedString("Location", comment: "")
+        }
+            
+        <<< ButtonRow(tagOffsetFromGMT) {
+            row in
+            row.title = NSLocalizedString("Offset from GMT", comment: "")
+        }
+        
+        <<< ButtonRow(tagAbbreviation) {
+            row in
+            row.title = NSLocalizedString("Abbreviation", comment: "")
+        }
+    }
 }
