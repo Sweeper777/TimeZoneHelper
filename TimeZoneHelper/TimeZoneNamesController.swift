924d73bb2cca1300b914bacbf5ab89a0c927dd66
Sweeper777 <sumulang@gmail.com>
2017-08-07 08:00:38 +0100

    implemented basic table view datasource methods

--- TimeZoneHelper/TimeZoneNamesController.swift
+++ TimeZoneHelper/TimeZoneNamesController.swift
@@ -2,4 +2,18 @@ import UIKit
 
 class TimeZoneNamesController: UITableViewController {
     let allTimeZoneNames = TimeZone.abbreviationDictionary.map { ($0.key, $0.value.replacingOccurrences(of: "_", with: " ")) }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTimeZoneNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(allTimeZoneNames[indexPath.row].0) (\(allTimeZoneNames[indexPath.row].1))"
+        return cell
+    }
 }
