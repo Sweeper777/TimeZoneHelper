a2728486fda1eb121909027d29cc86f4f73450ee
Sweeper777 <sumulang@gmail.com>
2017-08-11 11:25:43 +0100

    added TimeZoneSelectorRow

---
+++ TimeZoneHelper/TimeZoneSelectorRow.swift
@@ -0,0 +1,18 @@
+import Eureka
+
+class TimeZoneSelectorRow: SelectorRow<PushSelectorCell<TimeZone>, TimeZoneSelectorController> {
+    required init(tag: String?, _ initializer: ((TimeZoneSelectorRow) -> ())) {
+        super.init(tag: tag)
+        initializer(self)
+        presentationMode = PresentationMode.show(controllerProvider: ControllerProvider.storyBoard(storyboardId: "TimeZoneSelector", storyboardName: "Main", bundle: nil), onDismiss: {
+            _ in
+        })
+        displayValueFor = {
+            $0.identifier
+        }
+    }
+    
+    required convenience init(tag: String?) {
+        self.init(tag: tag)
+    }
+}
