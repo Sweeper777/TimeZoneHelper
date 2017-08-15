a4343c1b60809c14bc155d993cd3356e264c6320
Sweeper777 <sumulang@gmail.com>
2017-08-15 08:16:16 +0100

    added updateInfoRows helper method

--- TimeZoneHelper/TimeZoneInfoController.swift
+++ TimeZoneHelper/TimeZoneInfoController.swift
@@ -41,4 +41,32 @@ class TimeZoneInfoController: FormViewController {
         addInfoRow(tag: tagDSTOffset, title: "Current DST Offset")
         addInfoRow(tag: tagNextDSTTransition, title: "Next DST Transition")
     }
+    
+    func updateInfoRows() {
+        let row: RowOf<TimeZone>? = form.rowBy(tag: tagTimeZone)
+        guard let timeZone = row?.value else { return }
+        func setInfoRow(withTag tag: String, value: String) {
+            let infoRow: RowOf<String> = form.rowBy(tag: tag)!
+            infoRow.value = value
+            infoRow.updateCell()
+        }
+        setInfoRow(withTag: tagName, value: timeZone.identifier)
+        setInfoRow(withTag: tagAbbreviation, value: timeZone.abbreviation() ?? NSLocalizedString("N/A", comment: ""))
+        let numberFormatter = NumberFormatter()
+        numberFormatter.maximumFractionDigits = 2
+        let gmtOffset = Double(timeZone.secondsFromGMT()) / 60.0 / 60.0
+        setInfoRow(withTag: tagOffsetFromGMT, value: "\(numberFormatter.string(from: gmtOffset as NSNumber)!) \(NSLocalizedString("Hour(s)", comment: ""))")
+        
+        setInfoRow(withTag: tagIsDST, value: NSLocalizedString(timeZone.isDaylightSavingTime() ? "Yes" : "No", comment: ""))
+        let dstOffset = timeZone.daylightSavingTimeOffset() / 60.0 / 60.0
+        setInfoRow(withTag: tagDSTOffset, value: "\(numberFormatter.string(from: dstOffset as NSNumber)!) \(NSLocalizedString("Hour(s)", comment: ""))")
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        if let transitionDate = timeZone.nextDaylightSavingTimeTransition {
+            setInfoRow(withTag: tagNextDSTTransition, value: dateFormatter.string(from: transitionDate))
+        } else {
+            setInfoRow(withTag: tagNextDSTTransition, value: NSLocalizedString("N/A", comment: ""))
+        }
+    }
 }
