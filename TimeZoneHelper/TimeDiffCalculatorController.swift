706bee5c461a38591d8367e4ffc89c15085e56ea
Sweeper777 <sumulang@gmail.com>
2017-08-17 08:08:22 +0100

    added start date and time row

--- TimeZoneHelper/TimeDiffCalculatorController.swift
+++ TimeZoneHelper/TimeDiffCalculatorController.swift
@@ -2,6 +2,15 @@ import Eureka
 
 class TimeDiffCalculatorController: FormViewController {
     override func viewDidLoad() {
+        super.viewDidLoad()
+        let now = Date()
         
+        form +++ Section(NSLocalizedString("start date", comment: ""))
+        
+        <<< DateTimeRow(tagStartDateTime) {
+            row in
+            row.title = NSLocalizedString("Date & Time", comment: "")
+            row.value = now
+        }
     }
 }
