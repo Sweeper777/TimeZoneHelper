4d4f962c58ce1deed7e0aa4e9f9bcea118f1a95c
Sweeper777 <sumulang@gmail.com>
2017-08-18 07:51:18 +0100

    added end date time row

--- TimeZoneHelper/TimeDiffCalculatorController.swift
+++ TimeZoneHelper/TimeDiffCalculatorController.swift
@@ -18,5 +18,13 @@ class TimeDiffCalculatorController: FormViewController {
             row.title = NSLocalizedString("Time Zone", comment: "")
             row.value = TimeZone.current
         }
+        
+        form +++ Section(NSLocalizedString("end date", comment: ""))
+        
+        <<< DateTimeRow(tagEndDateTime) {
+            row in
+            row.title = NSLocalizedString("Date & Time", comment: "")
+            row.value = now.addingTimeInterval(3600)
+        }
     }
 }
